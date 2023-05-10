5ac6f0adc2206abce0e2dd04c4836151f4c2e250
Precious00001 <lanremidelola@gmail.com>
2023-05-09 19:24:11 -0700

    alx

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-for first_digit in range(0, 10):
-    for second_digit in range(first_digit + 1, 10):
-        if first_digit == 8 and second_digit == 9:
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
