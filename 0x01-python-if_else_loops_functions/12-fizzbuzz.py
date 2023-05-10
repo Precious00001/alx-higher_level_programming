7083f6bea50a04614c740dbc2821a5b6ae763854
Precious00001 <lanremidelola@gmail.com>
2023-05-09 19:55:54 -0700

    Alx

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,10 @@
 # 12-fizzbuzz.py
 
 
-def fizzbuzz()
+def fizzbuzz():
+
     for num in range(1, 101):
+
         if num % 3 == 0 and num % 5 == 0:
             print("FizzBuzz ", end="")
         elif num % 3 == 0:
