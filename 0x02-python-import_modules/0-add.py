84d108df87dfdd1fa2a281e9ad1ecf2c7f3c2a98
Precious00001 <lanremidelola@gmail.com>
2023-05-11 19:47:58 -0700

    alx

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: integer 1
+        b: integer 2
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition function
+
+    Args:
+        a: integer 1
+        b: integer 2
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """Subtraction function
+
+    Args:
+        a: integer 1
+        b: integer 2
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """Multiplication function
+
+    Args:
+        a: integer 1
+        b: integer 2
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: integer 1
+        b: integer 2
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
+
