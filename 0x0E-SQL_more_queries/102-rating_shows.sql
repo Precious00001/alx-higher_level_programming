6900e32346bfb64641f70c43b6eb284a6fe9fd33
root <lanremidelola@gmail.com>
2023-07-19 20:49:54 -0700

    alx

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+--  a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
