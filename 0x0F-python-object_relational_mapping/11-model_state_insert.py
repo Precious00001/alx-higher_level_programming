179195965329c233d7ecc2df1d9a72463f7ded2e
root <lanremidelola@gmail.com>
2023-08-21 16:05:49 -0700

    alx

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -12,13 +12,18 @@ if __name__ == "__main__":
     Access to the database and get a state
     from the database.
     """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state_n = State(name='Louisiana')
-    session.add(state_n)
-    instance_new = session.query(State).filter_by(name='Louisiana').first()
-    print(instance_new.id)
+    engine = create_engine(
+                            'mysql+mysqldb://{}:{}@localhost/{}'
+                            .format(
+                                        sys.argv[1],
+                                        sys.argv[2],
+                                        sys.argv[3]
+                                            ),
+                            pool_pre_ping=True
+                                )
+    session = Session(engine)
+    State_new = State(name="Louisiana")
+    re = session.add(State_new)
     session.commit()
+    print(State_new.id)
+    session.close()
