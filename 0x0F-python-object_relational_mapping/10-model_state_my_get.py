868431a0574b153576c2ecc02327fdaf0e841d9d
root <lanremidelola@gmail.com>
2023-08-21 03:34:25 -0700

    alx

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    instance = session.query(State).filter(State.name == argv[4]).first()
     if instance is None:
         print('Not found')
     else:
