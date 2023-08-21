4a928253163aea92e479c490b745a9d9d362076c
root <lanremidelola@gmail.com>
2023-08-21 16:33:08 -0700

    alx

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,7 +2,7 @@
 """
 This script defines a City class
 to work with MySQLAlchemy ORM.
-""" 
+"""
 from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 
