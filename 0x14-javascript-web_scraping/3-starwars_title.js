765e51c14c25c7dc13c0fd5361382c8dc8015c82
root <lanremidelola@gmail.com>
2023-09-26 17:53:39 -0700

    alx

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,30 +1,17 @@
 #!/usr/bin/node
 
-// Import the 'request' module to make HTTP requests
 const request = require('request');
-
-// Get the episode number from the command line arguments
 const episodeNum = process.argv[2];
-
-// Define the URL of the Star Wars API
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
 // Make an HTTP GET request to the API using the specified episode number
 request(API_URL + episodeNum, function (err, response, body) {
-  // Check for errors in the request
   if (err) {
     console.log(err);
-  } 
-  // Check if the response status code is 200 (OK)
-  else if (response.statusCode === 200) {
-    // Parse the response body as JSON
+  } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
-    
-    // Output the title of the film from the JSON response
     console.log(responseJSON.title);
-  } 
-  // Handle other response status codes (e.g., error)
-  else {
+  } else {
     console.log('Error code: ' + response.statusCode);
   }
 });
